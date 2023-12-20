0f633febbd3040d043171f666f05cdee9b12f4e6
Geexmo <you@example.com>
2023-12-20 19:43:22 +0300

    Change UML

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const sequelize = require('./db-connector');
 const cors = require('cors');
 const router = require('./routes/index');
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 5000;
 
 const app = express();
 app.use(cors());
